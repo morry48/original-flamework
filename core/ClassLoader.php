66a09a9361ab785f9fcb6800f1431e57b6fb9368
morry48 <yyyyuuuuuuuu0408@gmail.com>
2020-02-22 15:02:08 +0900

    defaltの読み込み設定

---
+++ core/ClassLoader.php
@@ -0,0 +1,27 @@
+<?php 
+
+Class ClassLoader
+{
+    protected $dirs;
+
+    public function register()
+    {
+        spl_autoload_register(array($this, 'loadClass'));
+    }
+
+    public function registerDir($dir)
+    {
+        $this->$dir[] = $dir;
+    }
+
+    public function loadClass($class)
+    {
+        foreach($this->dirs as $dir){
+            $file = $dir.'/'.$class.'.php';
+            if(is_readable($file)){
+                require $file;
+                return;
+            }
+        }
+    }
+}
