92c3b2dda57d327b30a7483021595c0073ee639f
morry48 <yyyyuuuuuuuu0408@gmail.com>
2020-02-25 00:12:12 +0900

    新規ユーザー登録の編集

--- controllers/AccountController.php
+++ controllers/AccountController.php
@@ -3,6 +3,8 @@ class AccountController extends Controller
 {
     public function signupAction() {
         return $this->render(array(
+            'user_name' => '',
+            'password' => '',
             '_token' => $this->generateCsrfToken('account/signup'),
         ));
     }
