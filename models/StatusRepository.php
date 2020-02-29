1d6ab09186f37bb02667783aaf5ac732d1e57c35
morry48 <yyyyuuuuuuuu0408@gmail.com>
2020-03-01 00:30:21 +0900

    ブログの表示内容の変更

--- models/StatusRepository.php
+++ models/StatusRepository.php
@@ -22,10 +22,10 @@ class StatusRepository extends DbRepository
     {
         $sql = "
             SELECT a.*, u.user_name
-                FROM status a
-                    LEFT JOIN user u ON a.user_id = u.id
-                    LEFT JOIN following f ON f.following = a.user_id
-                        AND f.user_id = :user_id
+            FROM status a
+                LEFT JOIN user u ON a.user_id = u.id
+                LEFT JOIN following f ON f.following_id = a.user_id
+                    AND f.user_id = :user_id
                 WHERE f.user_id = :user_id OR u.id = :user_id
                 ORDER BY a.created_at DESC
         ";
